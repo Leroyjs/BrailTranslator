bc5d4b91b5d34e4a060403dfb52eb3be7e299d0a
Никита <nikita@imac-dmitrij.ravoskhod.local>
2021-05-19 15:35:35 +0500

    mvp

--- App.js
+++ App.js
@@ -1,21 +1,124 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+    StyleSheet,
+    Pressable,
+    Vibration,
+    View,
+    Text,
+    Image,
+    Dimensions,
+    Button,
+    ScrollView,
+    TextInput,
+} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import React, { useEffect, useRef, useState } from 'react';
+
+import { dictionaryRus } from './dictionary';
+
+const App = () => {
+    const [word, setWord] = useState('');
+    console.log(word);
+    const arrayOfBraille = stringToArrayBraille(word);
+
+    return (
+        <View style={styles.wrapper}>
+            <TextInput
+                onChangeText={(text) => setWord(text)}
+                style={styles.input}
+            />
+            <View style={styles.imgWrapper}>
+                <Text>SomeIMG</Text>
+            </View>
+            <ScrollView
+                alwaysBounceHorizontal={true}
+                horizontal={true}
+                style={styles.line}
+            >
+                <View style={styles.lineInner}>
+                    {arrayOfBraille.map((item, index) => (
+                        <LetterInBraille
+                            key={item + index}
+                            brailleEncoding={item}
+                        />
+                    ))}
+                </View>
+            </ScrollView>
+        </View>
+    );
+};
+
+const LetterInBraille = ({ brailleEncoding }) => {
+    return (
+        <View style={styles.letterWrapper}>
+            {brailleEncoding.map((item, index) => (
+                <View
+                    key={index}
+                    style={{
+                        ...styles.letterDot,
+                        backgroundColor: item && '#000',
+                    }}
+                ></View>
+            ))}
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    wrapper: {
+        backgroundColor: '#ff0',
+        position: 'relative',
+        flex: 1,
+    },
+    input: {
+        height: 40,
+        margin: 12,
+        marginTop: 50,
+        borderWidth: 1,
+    },
+    line: {
+        maxHeight: 250,
+        backgroundColor: '#999',
+    },
+    letterDot: {
+        marginVertical: 15,
+        marginHorizontal: 25,
+        height: 20,
+        width: 20,
+    },
+    lineInner: { flexDirection: 'row' },
+    imgWrapper: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    letterWrapper: {
+        justifyContent: 'space-around',
+        flexWrap: 'wrap',
+        backgroundColor: '#0f0',
+        height: 200,
+        width: 140,
+        borderColor: '#000',
+        borderWidth: 1,
+        borderStyle: 'solid',
+    },
 });
+
+function stringToArrayBraille(string) {
+    const arrayOfLetter = string.split('');
+    const arrayOfBraille = arrayOfLetter.map((item) => {
+        return getLetterInBraille(item.toLowerCase(), dictionaryRus);
+    });
+    return arrayOfBraille;
+}
+
+function getLetterInBraille(letter, dictionary = []) {
+    let brailleEncoding = false;
+    dictionary.forEach((element) => {
+        if (letter === element.letter) {
+            brailleEncoding = element.brailleEncoding;
+        }
+    });
+    return brailleEncoding;
+}
+
+export default App;
---
+++ dictionary.js
@@ -0,0 +1,136 @@
+const dictionaryRus = [
+    {
+        letter: 'а',
+        brailleEncoding: [true, false, false, false, false, false], //
+    },
+    {
+        letter: 'б',
+        brailleEncoding: [true, true, false, false, false, false], //
+    },
+    {
+        letter: 'в',
+        brailleEncoding: [false, true, false, true, true, true], //
+    },
+    {
+        letter: 'г',
+        brailleEncoding: [true, true, false, true, true, false], //
+    },
+    {
+        letter: 'д',
+        brailleEncoding: [true, false, false, true, true, false], //
+    },
+    {
+        letter: 'е',
+        brailleEncoding: [true, false, false, false, true, false], //
+    },
+    {
+        letter: 'ё',
+        brailleEncoding: [true, false, false, false, false, true], //
+    },
+    {
+        letter: 'ж',
+        brailleEncoding: [false, true, false, true, true, false], //
+    },
+    {
+        letter: 'з',
+        brailleEncoding: [true, false, true, false, true, true], //
+    },
+    {
+        letter: 'и',
+        brailleEncoding: [false, true, false, true, false, false], //
+    },
+    {
+        letter: 'й',
+        brailleEncoding: [true, true, true, true, false, true], //
+    },
+    {
+        letter: 'к',
+        brailleEncoding: [true, false, true, false, false, false], //
+    },
+    {
+        letter: 'л',
+        brailleEncoding: [true, true, true, false, false, false], //
+    },
+    {
+        letter: 'м',
+        brailleEncoding: [true, false, true, true, false, false], //
+    },
+    {
+        letter: 'н',
+        brailleEncoding: [true, false, true, true, true, false], //
+    },
+    {
+        letter: 'о',
+        brailleEncoding: [true, false, true, false, true, false], //
+    },
+    {
+        letter: 'п',
+        brailleEncoding: [true, true, true, true, false, false], //
+    },
+    {
+        letter: 'р',
+        brailleEncoding: [true, true, true, false, true, false], //
+    },
+    {
+        letter: 'с',
+        brailleEncoding: [false, true, true, true, false, false],
+    },
+    {
+        letter: 'т',
+        brailleEncoding: [false, true, true, true, true, false],
+    },
+    {
+        letter: 'у',
+        brailleEncoding: [true, false, true, false, false, true], //
+    },
+    {
+        letter: 'ф',
+        brailleEncoding: [true, true, false, true, false, false], //
+    },
+    {
+        letter: 'х',
+        brailleEncoding: [true, true, false, false, true, false],
+    },
+    {
+        letter: 'ц',
+        brailleEncoding: [true, false, false, true, false, false], //
+    },
+    {
+        letter: 'ч',
+        brailleEncoding: [true, true, true, true, true, false],
+    },
+    {
+        letter: 'ш',
+        brailleEncoding: [true, false, false, false, true, true], //
+    },
+    {
+        letter: 'щ',
+        brailleEncoding: [true, false, true, true, false, true], //
+    },
+    {
+        letter: 'ъ',
+        brailleEncoding: [true, true, true, false, true, true],
+    },
+    {
+        letter: 'ы',
+        brailleEncoding: [false, true, true, true, false, true],
+    },
+    {
+        letter: 'ь',
+        brailleEncoding: [false, true, true, true, true, true],
+    },
+    {
+        letter: 'э',
+        brailleEncoding: [false, true, false, true, false, true],
+    },
+    {
+        letter: 'ю',
+        brailleEncoding: [true, true, false, false, true, true],
+    },
+    {
+        letter: 'я',
+        brailleEncoding: [true, true, false, true, false, true],
+    },
+];
+
+export { dictionaryRus };
